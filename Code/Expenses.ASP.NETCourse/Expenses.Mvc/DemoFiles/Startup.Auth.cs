3515bf2eed7da2e44e2168f91ed47c61cf28c8c0
GeekTrainer <charrison@geektrainer.com>
2015-02-12 08:32:09 -0800

    Initial Upload

---
+++ Code/Expenses.ASP.NETCourse/Expenses.Mvc/DemoFiles/Startup.Auth.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Expenses.Web
+{
+    public partial class Startup
+    {
+        //
+        // The Client ID is used by the application to uniquely identify itself to Azure AD.
+        // The Metadata Address is used by the application to retrieve the signing keys used by Azure AD.
+        // The AAD Instance is the instance of Azure, for example public Azure or Azure China.
+        // The Authority is the sign-in URL of the tenant.
+        // The Post Logout Redirect Uri is the URL where the user will be redirected after they sign out.
+        //
+        private static string clientId = ConfigurationManager.AppSettings["aad:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["aad:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["aad:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["aad:PostLogoutRedirectUri"];
+
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri
+                });
+        }
+    }
+}
